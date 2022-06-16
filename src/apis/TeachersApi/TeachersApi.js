7ba61848849cf36439d7f72433204e911f52ff44
masumwbdev <masumwbd@gmail.com>
2022-06-16 13:09:37 +0600

    added search filter

--- src/apis/TeachersApi/TeachersApi.js
+++ src/apis/TeachersApi/TeachersApi.js
@@ -2,21 +2,45 @@ import React, { useEffect, useState } from 'react';
 import AllTeachers from '../../components/AllTeachers/AllTeachers';
 
 const TeachersApi = () => {
+    const [loading, setLoading] = useState(false);
+    const [searchData, setSearchData] = useState("");
     const [teachers, setTeachers] = useState([]);
     useEffect(() => {
-        fetch('teachers_info.json')
-            .then(res => res.json())
-            .then(data => setTeachers(data))
+        const loadPost = async () => {
+            setLoading(true)
+            fetch('teachers_info.json')
+                .then(res => res.json())
+                .then(data => setTeachers(data))
+            setLoading(false);
+        }
+        loadPost();
     }, [])
     return (
-        <div class="mb-5 row row-cols-2 row-cols-md-5 g-3">
-            {
-                teachers.map(t => <AllTeachers
-                    key={t.id}
-                    t={t}
-                ></AllTeachers>)
-            }
-        </div>
+        <>
+            <div className='d-flex align-items-center justify-content-between mb-3'>
+                <div>
+                    <h5 className='fw-bold'>শিক্ষকদের তালিকা : </h5>
+                </div>
+                <div>
+                    <input className='border-0 rounded p-1' onChange={(e) => setSearchData(e.target.value)} type="text" placeholder='Search teacher...' />
+                </div>
+            </div>
+            <div class="mb-5 row row-cols-2 row-cols-md-5 g-3">
+                {loading ? (<h4>Loading...</h4>) :
+                    (teachers.filter((value) => {
+                        if (searchData === "") {
+                            return value;
+                        } else if (value.name.toLowerCase().includes(searchData.toLowerCase()) || value.title.includes(searchData) || value.phone.includes(searchData) || value.joined.includes(searchData) || value.district.includes(searchData)) {
+                            return value;
+                        }
+                    })
+                        .map(t => <AllTeachers
+                            key={t.id}
+                            t={t}
+                        ></AllTeachers>))
+                }
+            </div>
+        </>
     );
 };
 
